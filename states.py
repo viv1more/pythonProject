d1283526ad7d1d385a33149641d0382649e95c65
vivek <vivek788more@gmail.com>
2023-12-08 11:38:37 +0530

    formatted ,

---
+++ states.py
@@ -0,0 +1,17 @@
+print("states of india")
+
+states = [
+    "1.Andhra Pradesh", "2.Arunachal Pradesh", "3.Assam", "4.Bihar",
+    "5.Chhattisgarh", "6.Goa", "7.Gujarat", "8.Haryana", "9.Himachal Pradesh",
+    "10.Jammu",
+    "11.Jharkhand", "12.Karnataka", "13.Kerala", "14.Madhya Pradesh",
+    "15.Maharashtra", "16.Manipur", "17.Meghalaya", "18.Mizoram",
+    "19.Nagaland", "20.Odisha", "21.Punjab", "22.Rajasthan", "23.Sikkim",
+    "24.Tamil Nadu", "25.Telangana", "26.Tripura", "27.Uttar Pradesh",
+    "28.Uttarakhand", "29.West Bengal", "30.Andaman and Nicobar Islands",
+    "31.Chandigarh", "32.Dadra and Nagar Haveli", "33.Daman and Diu",
+    "34.Delhi", "35.Lakshadweep", "36.Puducherry", "37.Jammu and Kashmir",
+    "38.Ladakh", "39.Lakshadweep", "40.NCT of Delhi"
+]
+
+print(states[39])

2b2cac966aaaf9f1ec4d09077eeb995528f57904
Vivek More <vivek788more@gmail.com>
2024-01-03 11:57:21 +0530

    added files

---
+++ Dictionaries/nesting-dict.py
@@ -0,0 +1,10 @@
+# Nesting of dictionary in dictionary
+
+travelled = {
+    "India": {"Maharashtra": ["Jalgaon", "Nashik", "Pune", "Sambhajinagar", "Mumbai", "Dhule", "Durban"]}
+}
+
+print(travelled)
+
+#nesting a Dictionary in a List
+
